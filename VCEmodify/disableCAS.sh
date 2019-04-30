853b4ae56ed54e99b76c502acffb20707ead84bb
Ubuntu <ubuntu@ip-172-31-39-183.us-west-2.compute.internal>
2019-04-30 15:19:54 +0000

    vocab for VT. dockerfile hack. script fix.

--- VCEmodify/disableCAS.sh
+++ VCEmodify/disableCAS.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#security.cas.bypass=true
-f=$(grep -r -e "security.cas.bypass=true" | cut -d: -f1 | sort -d | uniq | grep -e "diableCAS.sh" -v | xargs)
+#security.cas.bypass=false
+f=$(grep -r -e "security.cas.bypass=false" | cut -d: -f1 | sort -d | uniq | grep -e "diableCAS.sh" -v | xargs)
 for i in $f; do
-        echo "sed -i 's,security.cas.bypass=true,security.cas.bypass=true,g' $i"
-        sed -i 's,security.cas.bypass=true,security.cas.bypass=true,g' $i
+        echo "sed -i 's,security.cas.bypass=false,security.cas.bypass=true,g' $i"
+        sed -i 's,security.cas.bypass=false,security.cas.bypass=true,g' $i
 done
 
-#disableCAS=true
-f=$(grep -r -e "disableCAS=true" | cut -d: -f1 | sort -d | uniq | grep -e "diableCAS.sh" -v | xargs)
+#disableCAS=false
+f=$(grep -r -e "disableCAS=false" | cut -d: -f1 | sort -d | uniq | grep -e "diableCAS.sh" -v | xargs)
 for i in $f; do
-        echo "sed -i 's,disableCAS=true,disableCAS=true,g' $i"
-        sed -i 's,disableCAS=true,disableCAS=true,g' $i
+        echo "sed -i 's,disableCAS=false,disableCAS=true,g' $i"
+        sed -i 's,disableCAS=false,disableCAS=true,g' $i
 done
